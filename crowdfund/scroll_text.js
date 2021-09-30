041155b2d8d9071640a5c52ff97535870fbf5497
fumiyatake <fumiyatake@gmail.com>
2021-09-30 21:24:01 +0900

    クラファンページのテキスト出現タイミングを調整

--- crowdfund/scroll_text.js
+++ crowdfund/scroll_text.js
@@ -1,7 +1,7 @@
 var scrollAnimationElm = document.querySelectorAll('.sa');
 var scrollAnimationFunc = function() {
   for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 300;
+    var triggerMargin = 250;
     if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
       scrollAnimationElm[i].classList.add('show');
     }
